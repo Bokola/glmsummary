dcca7e00ba6a56b31526866fa448d3c233712297
Basil Okola <33178339+Bokola@users.noreply.github.com>
2023-11-22 10:52:16 +0300

    move external data to /inst/extdata/ and install() then call

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' DYME data
+#'
+#'
+#' @format ## `dyme1`
+#' A data frame with 1,199 rows and 11 columns of pregnant mice exposed to varying
+#'  dyme doses in litters (clusters):
+#' \describe{
+#'   \item{no}{no malformation}
+#'   \item{minor}{minor malformation}
+#'   \item{yes}{full malformation}
+#'   \item{id}{litter identification number}
+#'   \item{dose}{administered dosage (numeric)}
+#'   \item{dose_fct}{administered dosage (factor)}
+#'   \item{weight}{weight of fetus}
+#'   \item{litsz}{size of the litter}
+#'   ...
+#' }
+#' @source <Price et al. (1987).>
+"dyme1"
