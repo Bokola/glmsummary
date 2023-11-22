e598d89885b190fb2e1ba9d0eba17d2cb0e130ef
Basil Okola <33178339+Bokola@users.noreply.github.com>
2023-11-22 08:47:31 +0300

    document install_load_packages()

--- R/install_load_packages.R
+++ R/install_load_packages.R
@@ -1,3 +1,14 @@
+#' Check for availability of required packages, install and load
+#'
+#' @param pkg a character vector of packages you wish to install
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' install_load_packages()
+#' }
 install_load_packages <- function(pkg){
   for (i in seq(pkg)){
     if (!require(pkg[[i]], character.only = TRUE)) install.packages(
